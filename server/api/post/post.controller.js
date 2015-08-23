95be2f15c0863c6929cea369d110dfda4e13ae09
Xavi <xavi.pedrals@gmail.com>
2015-08-23 22:22:47 +0200

    crea posts

--- server/api/post/post.controller.js
+++ server/api/post/post.controller.js
@@ -2,6 +2,8 @@
 
 var _ = require('lodash');
 var Post = require('./post.model');
+var User = require('../user/user.model');
+var ObjectId = require('mongoose').Types.ObjectId;
 
 // Get list of posts
 exports.index = function(req, res) {
@@ -22,9 +24,34 @@ exports.show = function(req, res) {
 
 // Creates a new post in the DB.
 exports.create = function(req, res) {
-  Post.create(req.body, function(err, post) {
-    if(err) { return handleError(res, err); }
-    return res.status(201).json(post);
+  var user_username = req.body.user_username;
+
+  User.findOrCreate({"username": user_username}, function(err, user){
+    if (err) { return handleError(res, err); }
+    var userId = new ObjectId(user._id);
+    var dataAct = new Date();
+
+    var aux = {
+      user_id: userId,
+      user_name: user.name,
+      user_username: user.username,
+      user_photo: user.photo,
+      user_type: user.type,
+      hashtag: req.body.hashtag,
+      date: dataAct,
+      description: req.body.description,
+      description_ext: req.body.description_ext,
+      photo: req.body.photo,
+      deadline: req.body.deadline,
+      location: req.body.location,
+      age: req.body.age,
+      bases: req.body.bases,
+      type: req.body.type
+    }
+    Post.create(aux, function(err, post) {
+      if(err) { return handleError(res, err); }
+      return res.status(201).json(post);
+    });
   });
 };
 
--- server/api/user/user.model.js
+++ server/api/user/user.model.js
@@ -3,11 +3,13 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
+var findOrCreate = require('mongoose-findorcreate');
+
 var UserSchema = new Schema({
   name: String,
   username: String,
   photo: String,
   type: Boolean
-});
+}).plugin(findOrCreate);
 
 module.exports = mongoose.model('User', UserSchema);
