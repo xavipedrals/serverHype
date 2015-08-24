1d0d0f1d20002dfe1021e74e75c1dcb5e3c4bc4b
Xavi <xavi.pedrals@gmail.com>
2015-08-24 13:21:04 +0200

    posts recents funcionen, hi ha algun bug a l'hora de crear posts

--- server/api/post/post.controller.js
+++ server/api/post/post.controller.js
@@ -7,10 +7,42 @@ var ObjectId = require('mongoose').Types.ObjectId;
 
 // Get list of posts
 exports.index = function(req, res) {
-  Post.find(function (err, posts) {
-    if(err) { return handleError(res, err); }
-    return res.status(200).json(posts);
-  });
+  console.log('Hola');
+  console.log(req.query.date);
+  if (req.query.recent10 != undefined) {
+    // db.getCollection('posts').find({}).sort({date:-1}).limit(10);
+    // http://localhost:9000/api/posts?recent10=true
+    Post.find(function(err, post){
+      if(err) { return handleError(res, err); }
+      if(!post) { return res.status(404).send('Not Found'); }
+      return res.json(post);
+    }).sort({"date": -1}).limit(10);
+  }
+  else if (req.query.recent20 != undefined) {
+    Post.find(function(err, post){
+      if(err) { return handleError(res, err); }
+      if(!post) { return res.status(404).send('Not Found'); }
+      return res.json(post);
+    }).sort({"date": -1}).limit(20);
+  }
+  else if (req.query.date != undefined){
+    //Si hay una fecha devuelve todos a partir de la fecha
+    //Formato fecha 2015-08-23T20:21:35.241Z
+    var data = new Date(req.query.date);
+    // db.getCollection('posts').find({date: {$gte: ISODate('2015-08-23T20:21:35.241Z')}})
+    //la petició es http://localhost:9000/api/posts?date=2015-08-25T20:21:35.241Z
+    Post.find({"date": {"$gte": data}}, function (err, post) {
+      if(err) { return handleError(res, err); }
+      if(!post) { return res.status(404).send('Not Found'); }
+      return res.json(post);
+    });
+  } else {
+    //Si no hay parametros devuelve todos
+    Post.find(function (err, posts) {
+      if(err) { return handleError(res, err); }
+      return res.status(200).json(posts);
+    });
+  }
 };
 
 // Get a single post
@@ -26,8 +58,9 @@ exports.show = function(req, res) {
 exports.create = function(req, res) {
   var user_username = req.body.user_username;
 
-  User.findOrCreate({"username": user_username}, function(err, user){
+  User.find({"username": user_username}, function(err, user){
     if (err) { return handleError(res, err); }
+    if(!user) { return res.status(404).send('Not Found');}
     var userId = new ObjectId(user._id);
     var dataAct = new Date();
 
